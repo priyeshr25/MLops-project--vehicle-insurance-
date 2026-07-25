7f3a6bb1ba54a7504e0677824b4a7956905a7ff8
Priyesh Ranjan <priyeshr255@gmail.com>
2026-07-25 21:31:14 +0530

    project structure ready venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Priyesh Ranjan",
+    author_email="priyesh255@gmail.com",
+    packages=find_packages()
+)
